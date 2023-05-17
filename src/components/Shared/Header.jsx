164bcc42cec1025913d4269a3c521739174bca13
mahedihassantonay <mahedihassantonay@gmail.comm>
2023-05-17 22:30:24 +0600

    1st commit

---
+++ src/components/Shared/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1>headeer</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+
+const PrivateRoute = () => {
+    return (
+        <div>
+            <h1>private route</h1>
+        </div>
+    );
+};
+
+export default PrivateRoute;
